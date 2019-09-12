3be32502c1e08665e21ebac4e47538d4047ec538
MohamedElsayed24 <mohamedelsayedhussein0@gmail.com>
2019-09-12 12:39:59 +0200

    edit2

--- js/custom.js
+++ js/custom.js
@@ -1,98 +1,120 @@
 
+
 $(document).ready(function(){
+
+  let scrollToTop = $('.scroll-top');
+
+  scrollToTop.on('click', ()=>{
+    $('html, body').animate({
+      scrollTop: 0
+    }, 700)
+  })
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() >= 100) {
+      scrollToTop.fadeIn();
+    }else {
+      scrollToTop.fadeOut();
+    }
+  });
+  
+});
+
+
+// $(document).ready(function(){
     // menu toggle bars button script
     
-    var $header = $('header');
+    // var $header = $('header');
 
-    $('.togglebutton').click( function() {
-        $(this).toggleClass('active');
-        $header.toggleClass('active');
-    });
+    // $('.togglebutton').click( function() {
+    //     $(this).toggleClass('active');
+    //     $header.toggleClass('active');
+    // });
 
     // scroll event for navbar header
 
-    var zero = 0;
+    // var zero = 0;
 
-    $(window).scroll(function() {
-        $header.toggleClass('hide', $(window).scrollTop() > zero);
-        zero = $(window).scrollTop();
+    // $(window).scroll(function() {
+    //     $header.toggleClass('hide', $(window).scrollTop() > zero);
+    //     zero = $(window).scrollTop();
 
-        if ($(window).scrollTop() > 0) {
-          $header.addClass('scrolled');
-        } else {
-          $header.removeClass('scrolled');
-        }
-    });
+    //     if ($(window).scrollTop() > 0) {
+    //       $header.addClass('scrolled');
+    //     } else {
+    //       $header.removeClass('scrolled');
+    //     }
+    // });
 
 
 
   // popup free quote script
 
-    $('a#freeQuote').click(function(e){
-      e.preventDefault();
-      $('.modal-quote').fadeIn(500);
-    });
+    // $('a#freeQuote').click(function(e){
+    //   e.preventDefault();
+    //   $('.modal-quote').fadeIn(500);
+    // });
 
-    $('.modal-quote span.close').click(function(){
-      $('.modal-quote').fadeOut(500);
-    });
+    // $('.modal-quote span.close').click(function(){
+    //   $('.modal-quote').fadeOut(500);
+    // });
     
-    $('.modal-quote').click(function(){
-        $(this).fadeOut(500);
-    });
+    // $('.modal-quote').click(function(){
+    //     $(this).fadeOut(500);
+    // });
 
-    $('.modal-quote .popup-content').click(function(event){
-      event.stopPropagation();
-    });
+    // $('.modal-quote .popup-content').click(function(event){
+    //   event.stopPropagation();
+    // });
     
-    $(document).keydown(function(e){
-      if (e.keyCode == 27) {
-        $('.modal-quote').fadeOut(500);
-      }
-    });
+    // $(document).keydown(function(e){
+    //   if (e.keyCode == 27) {
+    //     $('.modal-quote').fadeOut(500);
+    //   }
+    // });
 
   // slick slider init
   
-    $('.slider-for').slick({
-      autoplay: true,
-      speed: 2500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: true,
-      asNavFor: '.slider-nav',
-      prevArrow: '.services-section .controllers .PrevArrow',
-			nextArrow: '.services-section .controllers .NextArrow'
-    });
-    $('.slider-nav').slick({
-      autoplay: false,
-      speed: 2500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      asNavFor: '.slider-for',
-      arrows: false,
-      dots: false,
-      focusOnSelect: true
-    });
+    // $('.slider-for').slick({
+    //   autoplay: true,
+    //   speed: 2500,
+    //   slidesToShow: 1,
+    //   slidesToScroll: 1,
+    //   arrows: true,
+    //   asNavFor: '.slider-nav',
+    //   prevArrow: '.services-section .controllers .PrevArrow',
+		// 	nextArrow: '.services-section .controllers .NextArrow'
+    // });
+    // $('.slider-nav').slick({
+    //   autoplay: false,
+    //   speed: 2500,
+    //   slidesToShow: 1,
+    //   slidesToScroll: 1,
+    //   asNavFor: '.slider-for',
+    //   arrows: false,
+    //   dots: false,
+    //   focusOnSelect: true
+    // });
 
-  });
+  // });
 
 //  init aos animation library
-  AOS.init();
+  // AOS.init();
 
   // float placholder of inputs
   
-  function stateCheck($formControl) {
-    if ($formControl.val().length > 0) {
-      $formControl.addClass('valid')
-    }else {
-      $formControl.removeClass('valid')
-    }
-  }
-  $(function () {
-    $('.form-control').on('focusout', function(){
-      stateCheck($(this));
-    })
-  })
+  // function stateCheck($formControl) {
+  //   if ($formControl.val().length > 0) {
+  //     $formControl.addClass('valid')
+  //   }else {
+  //     $formControl.removeClass('valid')
+  //   }
+  // }
+  // $(function () {
+  //   $('.form-control').on('focusout', function(){
+  //     stateCheck($(this));
+  //   })
+  // })
 
   // calendar picker
 
@@ -107,47 +129,47 @@ $(document).ready(function(){
 
   // log in script
 
-  const signUpButton = document.getElementById("signUp");
-  const signInButton = document.getElementById("signIn");
-  const container = document.getElementById("container-log");
+  let signUpButton = $("#signUp");
+  let signInButton = $("#signIn");
+  let container = $("#container-log");
 
-  signUpButton.addEventListener("click", () => {
-    container.classList.add("right-panel-active");
+  signUpButton.on("click", () => {
+    container.addClass("right-panel-active");
   });
 
-  signInButton.addEventListener("click", () => {
-    container.classList.remove("right-panel-active");
+  signInButton.on("click", () => {
+    container.removeClass("right-panel-active");
   });
 
 
-  $(document).ready(function(){
-    $('#send-message').click(function(){
-      $('.forget-pass').css({
-        'height': '100%',
-        'opacity': '1',
-        'visibility': 'visible'
-        });
-    });
+  // $(document).ready(function(){
+  //   $('#send-message').click(function(){
+  //     $('.forget-pass').css({
+  //       'height': '100%',
+  //       'opacity': '1',
+  //       'visibility': 'visible'
+  //       });
+  //   });
 
-    $('.close-forget-pass , .forget-pass ').click(function(){
-      $('.forget-pass').css({
-        'height': '0',
-        'opacity': '0',
-        'visibility': 'hidden'
-        });
-    });
+  //   $('.close-forget-pass , .forget-pass ').click(function(){
+  //     $('.forget-pass').css({
+  //       'height': '0',
+  //       'opacity': '0',
+  //       'visibility': 'hidden'
+  //       });
+  //   });
 
-    $('.forget-pass .box').click(function(event){
-      event.stopPropagation();
-    });
+  //   $('.forget-pass .box').click(function(event){
+  //     event.stopPropagation();
+  //   });
 
-    $(document).keydown(function(e) {
-      if (e.keyCode == 27 ) {
-        $('.forget-pass').css({
-          'height': '0',
-          'opacity': '0',
-          'visibility': 'hidden'
-        });
-      }
-    });
-  });
+  //   $(document).keydown(function(e) {
+  //     if (e.keyCode == 27 ) {
+  //       $('.forget-pass').css({
+  //         'height': '0',
+  //         'opacity': '0',
+  //         'visibility': 'hidden'
+  //       });
+  //     }
+  //   });
+  // });
